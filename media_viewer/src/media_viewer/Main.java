6bbd4f22454f814b3cc3afe3e9542959eb7dcdf7
Benjamin Nelson <33214860+bnelson1324@users.noreply.github.com>
2020-05-18 13:39:35 -0700

    a

--- media_viewer/src/media_viewer/Main.java
+++ media_viewer/src/media_viewer/Main.java
@@ -2,4 +2,21 @@ package media_viewer;
 
 public class Main {
 
+	
+	public static void main(String[] args) {
+		
+		init();
+		
+		
+		
+		// launch program
+		System.out.println(SettingsLoader.getSetting("rootStorageFolderLoc"));
+	}
+	
+	private static void init() {
+		// loads settings
+		SettingsLoader.loadSettings();
+	
+	}
+	
 }
